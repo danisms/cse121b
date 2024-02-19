6ccf376eff4707db443f46b2a73eae5de38b98f4
danisms <danielcopute@gmail.com>
2024-02-19 19:24:18 +0100

    added about game section.

--- my_project/scripts/main.js
+++ my_project/scripts/main.js
@@ -4,11 +4,13 @@
 // Global Variables
 const playGameBtn = document.querySelector('#playGameBtn');
 const scoresBtn = document.querySelector('#scoresBtn');
+const aboutBtn = document.querySelector('#aboutBtn');
 const exitBtn = document.querySelector('#exitBtn');
 
 // Event Listeners
 playGameBtn.addEventListener('click', playGameMenu);
 scoresBtn.addEventListener('click', gameScores);
+aboutBtn.addEventListener('click', ()=> open('about.html', target='_self'))
 exitBtn.addEventListener('click', () => {
     open('index.html', '_self');
     close();
